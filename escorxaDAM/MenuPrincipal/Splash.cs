fa73a43bf13c449e9ed298414445b0f6c3286ab6
TheGerylop <gerard.lopezsds@gmail.com>
2018-03-22 17:04:37 +0100

    login and splash created

---
+++ escorxaDAM/MenuPrincipal/Splash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuPrincipal
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void timerSplash_Tick(object sender, EventArgs e)
+        {
+            progressBar.Increment(5);
+            if(progressBar.Value == 100)
+            {
+                timerSplash.Stop();
+                Form frm = new Login();
+                frm.Show();
+                this.Hide();
+            }
+        }
+    }
+}
