0b835895a39dd2c001f03b5160303eb1a344b77d
TheGerylop <gerard.lopezsds@gmail.com>
2018-04-03 18:19:54 +0200

    dll actualitzada

--- escorxaDAM/ComunitatsAutonomes/ComunitatsAutonomes.Designer.cs
+++ escorxaDAM/ComunitatsAutonomes/ComunitatsAutonomes.Designer.cs
@@ -38,7 +38,7 @@
             // customTextBox1
             // 
             this.customTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.customTextBox1.Location = new System.Drawing.Point(248, 156);
+            this.customTextBox1.Location = new System.Drawing.Point(227, 186);
             this.customTextBox1.Name = "customTextBox1";
             this.customTextBox1.Options = CustomControl.CustomTextBox.Items.Text;
             this.customTextBox1.Placeholder = null;
@@ -49,7 +49,7 @@
             // customTextBox2
             // 
             this.customTextBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.customTextBox2.Location = new System.Drawing.Point(248, 202);
+            this.customTextBox2.Location = new System.Drawing.Point(227, 232);
             this.customTextBox2.Name = "customTextBox2";
             this.customTextBox2.Options = CustomControl.CustomTextBox.Items.Text;
             this.customTextBox2.Placeholder = null;
@@ -62,7 +62,7 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(123, 156);
+            this.label3.Location = new System.Drawing.Point(102, 186);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 16);
             this.label3.TabIndex = 43;
@@ -73,7 +73,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(123, 204);
+            this.label1.Location = new System.Drawing.Point(102, 234);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 16);
             this.label1.TabIndex = 44;
@@ -84,7 +84,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(475, 51);
+            this.label2.Location = new System.Drawing.Point(454, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(194, 20);
             this.label2.TabIndex = 45;
@@ -99,8 +99,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.customTextBox2);
             this.Controls.Add(this.customTextBox1);
+            this.Location = new System.Drawing.Point(0, 0);
             this.Name = "ComunitatsAutonomes";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.ComunitatsAutonomes_Load);
             this.Controls.SetChildIndex(this.btnActualitzar, 0);
             this.Controls.SetChildIndex(this.btnAfegir, 0);
