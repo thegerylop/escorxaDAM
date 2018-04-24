328a7041875d23ac063397f465e830f103c9dd77
Roger B <barrancosroger@gmail.com>
2018-04-24 18:01:32 +0200

    Refer form seguint el moqup

--- escorxaDAM/RecepcioBestiar/Recepcio.Designer.cs
+++ escorxaDAM/RecepcioBestiar/Recepcio.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.comboBox1 = new CustomControl.comboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -46,42 +45,44 @@
             this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
             this.label9 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.customTextBox4 = new CustomControl.CustomTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.customTextBox5 = new CustomControl.CustomTextBox();
+            this.customTextBox6 = new CustomControl.CustomTextBox();
+            this.customTextBox7 = new CustomControl.CustomTextBox();
+            this.comboBox4 = new CustomControl.comboBox();
+            this.label15 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(194, 185);
+            this.comboBox1.Location = new System.Drawing.Point(706, 128);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Reference = null;
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(109, 21);
             this.comboBox1.TabIndex = 38;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(54, 183);
+            this.label1.Location = new System.Drawing.Point(576, 248);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 20);
             this.label1.TabIndex = 39;
             this.label1.Text = "Usuari Receptor";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label2.Location = new System.Drawing.Point(86, 243);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
-            this.label2.TabIndex = 40;
-            this.label2.Text = "Proveidor";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label3.Location = new System.Drawing.Point(514, 186);
+            this.label3.Location = new System.Drawing.Point(74, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 20);
             this.label3.TabIndex = 41;
@@ -91,7 +92,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label4.Location = new System.Drawing.Point(518, 246);
+            this.label4.Location = new System.Drawing.Point(74, 170);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 20);
             this.label4.TabIndex = 42;
@@ -101,7 +102,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label5.Location = new System.Drawing.Point(74, 307);
+            this.label5.Location = new System.Drawing.Point(592, 126);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 20);
             this.label5.TabIndex = 43;
@@ -111,7 +112,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label6.Location = new System.Drawing.Point(54, 407);
+            this.label6.Location = new System.Drawing.Point(489, 170);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(197, 20);
             this.label6.TabIndex = 44;
@@ -121,7 +122,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label7.Location = new System.Drawing.Point(498, 405);
+            this.label7.Location = new System.Drawing.Point(489, 209);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(201, 20);
             this.label7.TabIndex = 45;
@@ -131,7 +132,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F);
-            this.label8.Location = new System.Drawing.Point(498, 304);
+            this.label8.Location = new System.Drawing.Point(74, 248);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(101, 20);
             this.label8.TabIndex = 46;
@@ -140,16 +141,16 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(194, 240);
+            this.comboBox2.Location = new System.Drawing.Point(706, 247);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Reference = null;
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.Size = new System.Drawing.Size(109, 21);
             this.comboBox2.TabIndex = 47;
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(194, 306);
+            this.comboBox3.Location = new System.Drawing.Point(194, 460);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Reference = null;
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
@@ -157,7 +158,7 @@
             // 
             // customTextBox1
             // 
-            this.customTextBox1.Location = new System.Drawing.Point(620, 186);
+            this.customTextBox1.Location = new System.Drawing.Point(194, 129);
             this.customTextBox1.Name = "customTextBox1";
             this.customTextBox1.Options = CustomControl.CustomTextBox.Items.Password;
             this.customTextBox1.Placeholder = null;
@@ -166,7 +167,7 @@
             // 
             // customTextBox2
             // 
-            this.customTextBox2.Location = new System.Drawing.Point(620, 246);
+            this.customTextBox2.Location = new System.Drawing.Point(194, 172);
             this.customTextBox2.Name = "customTextBox2";
             this.customTextBox2.Options = CustomControl.CustomTextBox.Items.Password;
             this.customTextBox2.Placeholder = null;
@@ -175,7 +176,7 @@
             // 
             // customTextBox3
             // 
-            this.customTextBox3.Location = new System.Drawing.Point(620, 304);
+            this.customTextBox3.Location = new System.Drawing.Point(194, 248);
             this.customTextBox3.Name = "customTextBox3";
             this.customTextBox3.Options = CustomControl.CustomTextBox.Items.Password;
             this.customTextBox3.Placeholder = null;
@@ -185,7 +186,7 @@
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(265, 406);
+            this.dateTimePicker2.Location = new System.Drawing.Point(706, 172);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(109, 20);
             this.dateTimePicker2.TabIndex = 53;
@@ -193,7 +194,7 @@
             // dateTimePicker3
             // 
             this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker3.Location = new System.Drawing.Point(706, 405);
+            this.dateTimePicker3.Location = new System.Drawing.Point(706, 209);
             this.dateTimePicker3.Name = "dateTimePicker3";
             this.dateTimePicker3.Size = new System.Drawing.Size(109, 20);
             this.dateTimePicker3.TabIndex = 54;
@@ -202,25 +203,152 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(54, 55);
+            this.label9.Location = new System.Drawing.Point(74, 40);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(124, 20);
+            this.label9.Size = new System.Drawing.Size(52, 20);
             this.label9.TabIndex = 55;
-            this.label9.Text = "Recepció Bestiar";
+            this.label9.Text = "REMO";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Location = new System.Drawing.Point(434, 167);
+            this.panel1.Location = new System.Drawing.Point(81, 319);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1, 173);
+            this.panel1.Size = new System.Drawing.Size(740, 1);
             this.panel1.TabIndex = 56;
+            // 
+            // customTextBox4
+            // 
+            this.customTextBox4.Location = new System.Drawing.Point(194, 211);
+            this.customTextBox4.Name = "customTextBox4";
+            this.customTextBox4.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox4.Placeholder = null;
+            this.customTextBox4.Size = new System.Drawing.Size(52, 20);
+            this.customTextBox4.TabIndex = 57;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label10.Location = new System.Drawing.Point(74, 211);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(69, 20);
+            this.label10.TabIndex = 58;
+            this.label10.Text = "Num. Lot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label2.Location = new System.Drawing.Point(74, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "DIB";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label11.Location = new System.Drawing.Point(74, 516);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 20);
+            this.label11.TabIndex = 60;
+            this.label11.Text = "Edat";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label12.Location = new System.Drawing.Point(74, 458);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 20);
+            this.label12.TabIndex = 61;
+            this.label12.Text = "Sexe";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label13.Location = new System.Drawing.Point(489, 404);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(71, 20);
+            this.label13.TabIndex = 62;
+            this.label13.Text = "DIB Mare";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label14.Location = new System.Drawing.Point(489, 461);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(37, 20);
+            this.label14.TabIndex = 63;
+            this.label14.Text = "Pare";
+            // 
+            // customTextBox5
+            // 
+            this.customTextBox5.Location = new System.Drawing.Point(194, 406);
+            this.customTextBox5.Name = "customTextBox5";
+            this.customTextBox5.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox5.Placeholder = null;
+            this.customTextBox5.Size = new System.Drawing.Size(150, 20);
+            this.customTextBox5.TabIndex = 64;
+            // 
+            // customTextBox6
+            // 
+            this.customTextBox6.Location = new System.Drawing.Point(194, 516);
+            this.customTextBox6.Name = "customTextBox6";
+            this.customTextBox6.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox6.Placeholder = null;
+            this.customTextBox6.Size = new System.Drawing.Size(52, 20);
+            this.customTextBox6.TabIndex = 65;
+            // 
+            // customTextBox7
+            // 
+            this.customTextBox7.Location = new System.Drawing.Point(665, 406);
+            this.customTextBox7.Name = "customTextBox7";
+            this.customTextBox7.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox7.Placeholder = null;
+            this.customTextBox7.Size = new System.Drawing.Size(150, 20);
+            this.customTextBox7.TabIndex = 66;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(665, 463);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Reference = null;
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 67;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(67, 346);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(59, 20);
+            this.label15.TabIndex = 68;
+            this.label15.Text = "Animal";
             // 
             // Recepcio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 604);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.customTextBox7);
+            this.Controls.Add(this.customTextBox6);
+            this.Controls.Add(this.customTextBox5);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.customTextBox4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.dateTimePicker3);
@@ -236,7 +364,6 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Recepcio";
@@ -244,7 +371,6 @@
             this.Controls.SetChildIndex(this.btnInserir, 0);
             this.Controls.SetChildIndex(this.comboBox1, 0);
             this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.label4, 0);
             this.Controls.SetChildIndex(this.label5, 0);
@@ -260,6 +386,18 @@
             this.Controls.SetChildIndex(this.dateTimePicker3, 0);
             this.Controls.SetChildIndex(this.label9, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.customTextBox4, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.label12, 0);
+            this.Controls.SetChildIndex(this.label13, 0);
+            this.Controls.SetChildIndex(this.label14, 0);
+            this.Controls.SetChildIndex(this.customTextBox5, 0);
+            this.Controls.SetChildIndex(this.customTextBox6, 0);
+            this.Controls.SetChildIndex(this.customTextBox7, 0);
+            this.Controls.SetChildIndex(this.comboBox4, 0);
+            this.Controls.SetChildIndex(this.label15, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -269,7 +407,6 @@
 
         private CustomControl.comboBox comboBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
@@ -285,5 +422,17 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker3;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Panel panel1;
+        private CustomControl.CustomTextBox customTextBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private CustomControl.CustomTextBox customTextBox5;
+        private CustomControl.CustomTextBox customTextBox6;
+        private CustomControl.CustomTextBox customTextBox7;
+        private CustomControl.comboBox comboBox4;
+        private System.Windows.Forms.Label label15;
     }
 }
