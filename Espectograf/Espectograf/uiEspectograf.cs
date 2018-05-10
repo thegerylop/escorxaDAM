d4c8be77130e578e139fff19382751e6dba1cbe1
Miquel <molla.miquel@gmail.com>
2018-05-10 18:25:39 +0200

    Documentacio espectrograf

--- Espectograf/Espectograf/uiEspectograf.cs
+++ Espectograf/Espectograf/uiEspectograf.cs
@@ -11,20 +11,27 @@ namespace Espectograf
 {
     public partial class uiEspectograf : Form
     {
-        float valorGauge1, valorGauge2, valorGauge3,weight;
+        float valorGauge1, valorGauge2, valorGauge3, weight;
         string ruta;
         private static IPEndPoint connPoint = null;
         private static IPAddress localAdd = IPAddress.Any;
         private static int PORT_NO = 5000;
-
+        /// <summary>
+        /// Inicialitza el form. Incialitza el thread i crea el punt d'escolta
+        /// </summary>
         public uiEspectograf()
         {
             InitializeComponent();
             connPoint = new IPEndPoint(localAdd, PORT_NO);
             new Thread(listener).Start();
         }
-
-        public string llegirArxiu(string ruta,string safata)
+        /// <summary>
+        /// Funció que llegueix un arxiu d'espectrògraf i aconseguir les dades
+        /// </summary>
+        /// <param name="ruta">Ruta on trobem l'arxiu</param>
+        /// <param name="safata">Tipus de safata que estem analitzant</param>
+        /// <returns>Retorna una string amb les 3 dades que demana el programa</returns>
+        public string llegirArxiu(string ruta, string safata)
         {
             string inici;
             string final;
@@ -35,7 +42,10 @@ namespace Espectograf
 
             int llargada = TotalLines(ruta);
             int linia = intRandom(llargada);
-            for(int i = 0; i < linia; i++) { texto = fsText.ReadLine(); }
+            for(int i = 0; i < linia; i++)
+            {
+                texto = fsText.ReadLine();
+            }
 
             //Agafo valor de gauge1
             inici = "CO";
@@ -84,43 +94,67 @@ namespace Espectograf
             fs_text.Close();
             return vGauge1.ToString() + "," + vGauge2.ToString() + "," + vGauge3.ToString();
         }
- 
+        /// <summary>
+        /// Funció que agafa una línia random de l'arxiu
+        /// </summary>
+        /// <param name="llargadaMAX">Quantitat de línies de l'arxiu</param>
+        /// <returns>Retorna un int que és una línia random</returns>
         private static int intRandom(int llargadaMAX)
         {
             Random linia = new Random();
             return linia.Next(0, llargadaMAX);
         }
-
+        /// <summary>
+        /// Aconsegueix el número total de línies que té un arxiu
+        /// </summary>
+        /// <param name="ruta">Ruta de l'arxiu a analitzar</param>
+        /// <returns>Número de línies de l'arxiu</returns>
         private static int TotalLines(string ruta)
         {
             using(StreamReader r = new StreamReader(ruta))
             {
                 int i = 0;
-                while(r.ReadLine() != null) { i++; }
+                while(r.ReadLine() != null)
+                {
+                    i++;
+                }
                 return i;
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="texto"></param>
+        /// <returns></returns>
         private float getValueFromString(string start, string end, string texto)
         {
             string dades = "";
             int inici, final, length;
 
-            if(texto == "") { return 0f; }
+            if (texto == "")
+            {
+                return 0f;
+            }
             else
             {
                 inici = texto.IndexOf(start) + start.Length;
 
                 //Si end no té contingut significa que el valor és MG
-                if(end != "")
+                if (end != "")
                 {
                     final = texto.IndexOf(end) - end.Length;
                     length = final - inici + 1;
                 }
-                else { length = 2; }
+                else
+                {
+                    length = 2;
+                }
 
                 //Agafo les dades
-                if(length > 1) {
+                if (length > 1)
+                {
                     dades = texto.Substring(inici, 2);
                     dades = (Int32.Parse(dades)).ToString();
                 }
@@ -128,7 +162,9 @@ namespace Espectograf
                 return float.Parse(dades);
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public void listener()
         {
             TcpListener listener = new TcpListener(localAdd, PORT_NO);
@@ -150,7 +186,12 @@ namespace Espectograf
                 nwStream.Write(buffer, 0, buffer.Length);
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pes"></param>
+        /// <param name="mg"></param>
+        /// <returns></returns>
         private float calculper100g(float pes, float mg)
         {
             float mgper100g;
@@ -166,7 +207,12 @@ namespace Espectograf
             mgper100g = mgper100g * 100;
             return mgper100g;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vg1"></param>
+        /// <param name="vg2"></param>
+        /// <param name="vg3"></param>
         public void updateGauges(float vg1, float vg2, float vg3)
         {
             aGauge1.Invoke((MethodInvoker)delegate { aGauge1.Value = 0f; });
