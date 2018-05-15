a36e82cbb8998afd00ff33710441f1f70730aa16
Roger B <barrancosroger@gmail.com>
2018-05-15 17:07:26 +0200

    Form processat inicial

---
+++ escorxaDAM/GestioProcessos/Processat Inicial.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace GestioProcessos
+{
+    partial class Processat_Inicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxUsuari = new CustomControl.CustomTextBox();
+            this.txtBoxLot = new CustomControl.CustomTextBox();
+            this.txtBoxId = new CustomControl.CustomTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.customTextBox1 = new CustomControl.CustomTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBoxUsuari
+            // 
+            this.txtBoxUsuari.Location = new System.Drawing.Point(686, 367);
+            this.txtBoxUsuari.Name = "txtBoxUsuari";
+            this.txtBoxUsuari.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxUsuari.Placeholder = null;
+            this.txtBoxUsuari.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxUsuari.TabIndex = 102;
+            this.txtBoxUsuari.Tag = "Nom";
+            // 
+            // txtBoxLot
+            // 
+            this.txtBoxLot.Location = new System.Drawing.Point(686, 255);
+            this.txtBoxLot.Name = "txtBoxLot";
+            this.txtBoxLot.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxLot.Placeholder = null;
+            this.txtBoxLot.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxLot.TabIndex = 101;
+            this.txtBoxLot.Tag = "Nom";
+            // 
+            // txtBoxId
+            // 
+            this.txtBoxId.Location = new System.Drawing.Point(172, 363);
+            this.txtBoxId.Name = "txtBoxId";
+            this.txtBoxId.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxId.Placeholder = null;
+            this.txtBoxId.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxId.TabIndex = 100;
+            this.txtBoxId.Tag = "Nom";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(587, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 16);
+            this.label8.TabIndex = 99;
+            this.label8.Text = "Pes del canal:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(467, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(213, 16);
+            this.label7.TabIndex = 98;
+            this.label7.Text = "Documentació d\'Identificació Boví:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 97;
+            this.label2.Text = "Numero Carril:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 96;
+            this.label1.Text = "Usuari:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(32, 37);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(121, 20);
+            this.label13.TabIndex = 104;
+            this.label13.Text = "Processat Inicial";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(291, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 90);
+            this.dataGridView1.TabIndex = 106;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(136, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 108);
+            this.panel1.TabIndex = 107;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 93;
+            this.label3.Text = "Material en espera";
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.Location = new System.Drawing.Point(172, 252);
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Options = CustomControl.CustomTextBox.Items.Text;
+            this.customTextBox1.Placeholder = null;
+            this.customTextBox1.Size = new System.Drawing.Size(161, 20);
+            this.customTextBox1.TabIndex = 108;
+            this.customTextBox1.Tag = "Nom";
+            // 
+            // Processat_Inicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 604);
+            this.Controls.Add(this.customTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtBoxUsuari);
+            this.Controls.Add(this.txtBoxLot);
+            this.Controls.Add(this.txtBoxId);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Processat_Inicial";
+            this.Text = "Processat_Inicial";
+            this.Controls.SetChildIndex(this.btnInserir, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.txtBoxId, 0);
+            this.Controls.SetChildIndex(this.txtBoxLot, 0);
+            this.Controls.SetChildIndex(this.txtBoxUsuari, 0);
+            this.Controls.SetChildIndex(this.label13, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.customTextBox1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CustomControl.CustomTextBox txtBoxUsuari;
+        private CustomControl.CustomTextBox txtBoxLot;
+        private CustomControl.CustomTextBox txtBoxId;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private CustomControl.CustomTextBox customTextBox1;
+    }
+}
