fed0df8add7b7ae8d3cef8b523700605974aeed4
Miquel <molla.miquel@gmail.com>
2018-05-15 18:05:55 +0200

    Emmagatzematge creat

---
+++ escorxaDAM/GestioProcessos/Emmagatzematge.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace GestioProcessos
+{
+    partial class Emmagatzematge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUserIn = new CustomControl.CustomTextBox();
+            this.txtUserOut = new CustomControl.CustomTextBox();
+            this.txtLot = new CustomControl.CustomTextBox();
+            this.txtMaxTemp = new CustomControl.CustomTextBox();
+            this.txtMinTemp = new CustomControl.CustomTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlSafates = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.comboEstat = new CustomControl.comboBox();
+            this.dateIn = new System.Windows.Forms.DateTimePicker();
+            this.dateOut = new System.Windows.Forms.DateTimePicker();
+            this.txtNumCamFrigo = new CustomControl.CustomTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlSafates.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnInserir.Location = new System.Drawing.Point(830, 592);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 70;
+            this.label1.Text = "Nº cambra frigorífica:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(231, 20);
+            this.label6.TabIndex = 68;
+            this.label6.Text = "Emmagatzematge i conservació";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 71;
+            this.label2.Text = "Usuari entrada:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(121, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 72;
+            this.label3.Text = "Usuari sortida:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(126, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 73;
+            this.label4.Text = "Data entrada:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(131, 448);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 74;
+            this.label5.Text = "Data sortida:";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(683, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 16);
+            this.label7.TabIndex = 75;
+            this.label7.Text = "Lot:";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(573, 323);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 16);
+            this.label8.TabIndex = 76;
+            this.label8.Text = "Temperatura màxima:";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(577, 364);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(135, 16);
+            this.label9.TabIndex = 77;
+            this.label9.Text = "Temperatura mínima:";
+            // 
+            // txtUserIn
+            // 
+            this.txtUserIn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtUserIn.Location = new System.Drawing.Point(227, 322);
+            this.txtUserIn.Name = "txtUserIn";
+            this.txtUserIn.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtUserIn.Placeholder = null;
+            this.txtUserIn.Size = new System.Drawing.Size(227, 20);
+            this.txtUserIn.TabIndex = 80;
+            this.txtUserIn.Tag = "Nom";
+            // 
+            // txtUserOut
+            // 
+            this.txtUserOut.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtUserOut.Location = new System.Drawing.Point(227, 363);
+            this.txtUserOut.Name = "txtUserOut";
+            this.txtUserOut.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtUserOut.Placeholder = null;
+            this.txtUserOut.Size = new System.Drawing.Size(227, 20);
+            this.txtUserOut.TabIndex = 81;
+            this.txtUserOut.Tag = "Nom";
+            // 
+            // txtLot
+            // 
+            this.txtLot.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtLot.Location = new System.Drawing.Point(726, 278);
+            this.txtLot.Name = "txtLot";
+            this.txtLot.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtLot.Placeholder = null;
+            this.txtLot.Size = new System.Drawing.Size(223, 20);
+            this.txtLot.TabIndex = 84;
+            this.txtLot.Tag = "Nom";
+            // 
+            // txtMaxTemp
+            // 
+            this.txtMaxTemp.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtMaxTemp.Location = new System.Drawing.Point(726, 322);
+            this.txtMaxTemp.Name = "txtMaxTemp";
+            this.txtMaxTemp.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtMaxTemp.Placeholder = null;
+            this.txtMaxTemp.Size = new System.Drawing.Size(223, 20);
+            this.txtMaxTemp.TabIndex = 85;
+            this.txtMaxTemp.Tag = "Nom";
+            // 
+            // txtMinTemp
+            // 
+            this.txtMinTemp.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtMinTemp.Location = new System.Drawing.Point(726, 363);
+            this.txtMinTemp.Name = "txtMinTemp";
+            this.txtMinTemp.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtMinTemp.Placeholder = null;
+            this.txtMinTemp.Size = new System.Drawing.Size(223, 20);
+            this.txtMinTemp.TabIndex = 86;
+            this.txtMinTemp.Tag = "Nom";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(227, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(722, 90);
+            this.dataGridView1.TabIndex = 91;
+            // 
+            // pnlSafates
+            // 
+            this.pnlSafates.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pnlSafates.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlSafates.Controls.Add(this.label13);
+            this.pnlSafates.Location = new System.Drawing.Point(72, 114);
+            this.pnlSafates.Name = "pnlSafates";
+            this.pnlSafates.Size = new System.Drawing.Size(890, 108);
+            this.pnlSafates.TabIndex = 92;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(3, 7);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(129, 20);
+            this.label13.TabIndex = 93;
+            this.label13.Text = "Safates pendents";
+            // 
+            // label14
+            // 
+            this.label14.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(593, 406);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(112, 16);
+            this.label14.TabIndex = 93;
+            this.label14.Text = "Estat refrigeració:";
+            // 
+            // comboEstat
+            // 
+            this.comboEstat.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboEstat.FormattingEnabled = true;
+            this.comboEstat.Location = new System.Drawing.Point(726, 406);
+            this.comboEstat.Name = "comboEstat";
+            this.comboEstat.Reference = null;
+            this.comboEstat.Size = new System.Drawing.Size(223, 21);
+            this.comboEstat.TabIndex = 96;
+            // 
+            // dateIn
+            // 
+            this.dateIn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dateIn.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateIn.Location = new System.Drawing.Point(227, 402);
+            this.dateIn.Name = "dateIn";
+            this.dateIn.Size = new System.Drawing.Size(227, 20);
+            this.dateIn.TabIndex = 97;
+            // 
+            // dateOut
+            // 
+            this.dateOut.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dateOut.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateOut.Location = new System.Drawing.Point(227, 444);
+            this.dateOut.Name = "dateOut";
+            this.dateOut.Size = new System.Drawing.Size(227, 20);
+            this.dateOut.TabIndex = 98;
+            // 
+            // txtNumCamFrigo
+            // 
+            this.txtNumCamFrigo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtNumCamFrigo.Location = new System.Drawing.Point(227, 278);
+            this.txtNumCamFrigo.Name = "txtNumCamFrigo";
+            this.txtNumCamFrigo.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtNumCamFrigo.Placeholder = null;
+            this.txtNumCamFrigo.Size = new System.Drawing.Size(227, 20);
+            this.txtNumCamFrigo.TabIndex = 99;
+            this.txtNumCamFrigo.Tag = "Nom";
+            // 
+            // Emmagatzematge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 717);
+            this.Controls.Add(this.txtNumCamFrigo);
+            this.Controls.Add(this.dateOut);
+            this.Controls.Add(this.dateIn);
+            this.Controls.Add(this.comboEstat);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMinTemp);
+            this.Controls.Add(this.txtMaxTemp);
+            this.Controls.Add(this.txtLot);
+            this.Controls.Add(this.txtUserOut);
+            this.Controls.Add(this.txtUserIn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pnlSafates);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Emmagatzematge";
+            this.Text = "Processat_Final";
+            this.Controls.SetChildIndex(this.pnlSafates, 0);
+            this.Controls.SetChildIndex(this.btnInserir, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.txtUserIn, 0);
+            this.Controls.SetChildIndex(this.txtUserOut, 0);
+            this.Controls.SetChildIndex(this.txtLot, 0);
+            this.Controls.SetChildIndex(this.txtMaxTemp, 0);
+            this.Controls.SetChildIndex(this.txtMinTemp, 0);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.label14, 0);
+            this.Controls.SetChildIndex(this.comboEstat, 0);
+            this.Controls.SetChildIndex(this.dateIn, 0);
+            this.Controls.SetChildIndex(this.dateOut, 0);
+            this.Controls.SetChildIndex(this.txtNumCamFrigo, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlSafates.ResumeLayout(false);
+            this.pnlSafates.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private CustomControl.CustomTextBox txtUserIn;
+        private CustomControl.CustomTextBox txtUserOut;
+        private CustomControl.CustomTextBox txtLot;
+        private CustomControl.CustomTextBox txtMaxTemp;
+        private CustomControl.CustomTextBox txtMinTemp;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnlSafates;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private CustomControl.comboBox comboEstat;
+        private System.Windows.Forms.DateTimePicker dateIn;
+        private System.Windows.Forms.DateTimePicker dateOut;
+        private CustomControl.CustomTextBox txtNumCamFrigo;
+    }
+}
