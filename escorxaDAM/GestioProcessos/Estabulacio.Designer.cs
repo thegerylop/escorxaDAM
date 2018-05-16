93ea57e8e1f3529b967def932fac6c06262bc5d2
Roger Barrancos <barrancosroger@gmail.com>
2018-05-16 18:23:20 +0200

    datagrid data done

---
+++ escorxaDAM/GestioProcessos/Estabulacio.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace GestioProcessos
+{
+    partial class Estabulacio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.estabulacioDataGrid = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new CustomControl.comboBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.customTextBox5 = new CustomControl.CustomTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customTextBox1 = new CustomControl.CustomTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.estabulacioDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(837, 621);
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // estabulacioDataGrid
+            // 
+            this.estabulacioDataGrid.AllowUserToAddRows = false;
+            this.estabulacioDataGrid.AllowUserToDeleteRows = false;
+            this.estabulacioDataGrid.AllowUserToResizeColumns = false;
+            this.estabulacioDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.estabulacioDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.estabulacioDataGrid.Location = new System.Drawing.Point(52, 77);
+            this.estabulacioDataGrid.Name = "estabulacioDataGrid";
+            this.estabulacioDataGrid.Size = new System.Drawing.Size(378, 172);
+            this.estabulacioDataGrid.TabIndex = 112;
+            this.estabulacioDataGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.estabulacioDataGrid_CellDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 111;
+            this.label3.Text = "Estabulació";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(201, 489);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Reference = null;
+            this.comboBox2.Size = new System.Drawing.Size(192, 21);
+            this.comboBox2.TabIndex = 101;
+            // 
+            // label14
+            // 
+            this.label14.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(476, 486);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(130, 20);
+            this.label14.TabIndex = 110;
+            this.label14.Text = "Dia/Hora Sortida :";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(476, 432);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(133, 20);
+            this.label13.TabIndex = 109;
+            this.label13.Text = "Dia/Hora entrada :";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dateTimePicker2.Location = new System.Drawing.Point(650, 486);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 104;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dateTimePicker1.Location = new System.Drawing.Point(650, 432);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 103;
+            // 
+            // customTextBox5
+            // 
+            this.customTextBox5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.customTextBox5.Location = new System.Drawing.Point(201, 546);
+            this.customTextBox5.Name = "customTextBox5";
+            this.customTextBox5.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox5.Placeholder = null;
+            this.customTextBox5.Size = new System.Drawing.Size(192, 20);
+            this.customTextBox5.TabIndex = 102;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 546);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 108;
+            this.label1.Text = "Incidencies :";
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.customTextBox1.Location = new System.Drawing.Point(201, 435);
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Options = CustomControl.CustomTextBox.Items.Password;
+            this.customTextBox1.Placeholder = null;
+            this.customTextBox1.Size = new System.Drawing.Size(192, 20);
+            this.customTextBox1.TabIndex = 100;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 489);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 20);
+            this.label5.TabIndex = 107;
+            this.label5.Text = "Usuari :";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 106;
+            this.label4.Text = "Numero estable :";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 105;
+            this.label2.Text = "Dades";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(52, 328);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 1);
+            this.panel1.TabIndex = 99;
+            // 
+            // Estabulacio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1126, 749);
+            this.Controls.Add(this.estabulacioDataGrid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.customTextBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.customTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Estabulacio";
+            this.Load += new System.EventHandler(this.Estabulacio_Load);
+            this.Controls.SetChildIndex(this.btnInserir, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.customTextBox1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.customTextBox5, 0);
+            this.Controls.SetChildIndex(this.dateTimePicker1, 0);
+            this.Controls.SetChildIndex(this.dateTimePicker2, 0);
+            this.Controls.SetChildIndex(this.label13, 0);
+            this.Controls.SetChildIndex(this.label14, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.estabulacioDataGrid, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.estabulacioDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView estabulacioDataGrid;
+        private System.Windows.Forms.Label label3;
+        private CustomControl.comboBox comboBox2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private CustomControl.CustomTextBox customTextBox5;
+        private System.Windows.Forms.Label label1;
+        private CustomControl.CustomTextBox customTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ escorxaDAM/GestioProcessos/Estabulacio.cs
@@ -0,0 +1,47 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GestioProcessos
+{
+    public partial class Estabulacio : BaseForm.BaseInserir
+    {
+
+        escorxadam2Entities _m = new escorxadam2Entities();
+
+        public Estabulacio()
+        {
+            InitializeComponent();
+        }
+
+        private void Estabulacio_Load(object sender, EventArgs e)
+        {
+            ActualitzarDataGrid();
+        }
+        private void ActualitzarDataGrid()
+        {
+            var num = (from a in _m.lots
+                       join b in _m.estables on a.idEstabulacio equals b.idEstable
+                       join c in _m.estat_estabulacions on b.idEstatEstabulacio equals c.idEstatEstabulacio
+                       where (c.nomEstatEstabulacio == "En espera" || c.nomEstatEstabulacio == "En procés")
+                       select new { a.numLot, c.nomEstatEstabulacio }).ToArray();
+            estabulacioDataGrid.DataSource = num;
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void estabulacioDataGrid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if()
+        }
+    }
+}
