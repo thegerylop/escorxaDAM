78541050524b6afb34e6e7167d9382ac11a67662
Roger B <barrancosroger@gmail.com>
2018-05-18 18:27:20 +0200

    Form Sacrifici

---
+++ escorxaDAM/GestioProcessos/Sacrifici.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace GestioProcessos
+{
+    partial class Sacrifici
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridAnimals = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cBoxAtordiment = new CustomControl.comboBox();
+            this.txtBoxBox = new CustomControl.CustomTextBox();
+            this.txtBoxEstat = new CustomControl.CustomTextBox();
+            this.txtBoxAparell = new CustomControl.CustomTextBox();
+            this.txtBoxUsuari = new CustomControl.CustomTextBox();
+            this.txtBoxIncidencies = new CustomControl.CustomTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBoxNLot = new CustomControl.CustomTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAnimals)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(837, 621);
+            // 
+            // gridAnimals
+            // 
+            this.gridAnimals.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.gridAnimals.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridAnimals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAnimals.Location = new System.Drawing.Point(190, 72);
+            this.gridAnimals.Name = "gridAnimals";
+            this.gridAnimals.Size = new System.Drawing.Size(306, 198);
+            this.gridAnimals.TabIndex = 109;
+            this.gridAnimals.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridAnimals_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(35, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(496, 218);
+            this.panel1.TabIndex = 110;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 93;
+            this.label3.Text = "Incoming RIP";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(36, 21);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(63, 20);
+            this.label13.TabIndex = 108;
+            this.label13.Text = "Sacrifici";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(492, 472);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 16);
+            this.label8.TabIndex = 114;
+            this.label8.Text = "Estat:";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(418, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 16);
+            this.label7.TabIndex = 113;
+            this.label7.Text = "Identficador de l\'aparell:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 112;
+            this.label2.Text = "Usuari:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 111;
+            this.label1.Text = "Box d\'immovilització:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 532);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 16);
+            this.label5.TabIndex = 121;
+            this.label5.Text = "Sistema d\'atordiment:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 123;
+            this.label4.Text = "Dades";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(40, 348);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(740, 1);
+            this.panel2.TabIndex = 124;
+            // 
+            // cBoxAtordiment
+            // 
+            this.cBoxAtordiment.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cBoxAtordiment.FormattingEnabled = true;
+            this.cBoxAtordiment.Location = new System.Drawing.Point(183, 532);
+            this.cBoxAtordiment.Name = "cBoxAtordiment";
+            this.cBoxAtordiment.Reference = null;
+            this.cBoxAtordiment.Size = new System.Drawing.Size(161, 21);
+            this.cBoxAtordiment.TabIndex = 122;
+            // 
+            // txtBoxBox
+            // 
+            this.txtBoxBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxBox.Location = new System.Drawing.Point(183, 402);
+            this.txtBoxBox.Name = "txtBoxBox";
+            this.txtBoxBox.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxBox.Placeholder = null;
+            this.txtBoxBox.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxBox.TabIndex = 118;
+            this.txtBoxBox.Tag = "Nom";
+            // 
+            // txtBoxEstat
+            // 
+            this.txtBoxEstat.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxEstat.Location = new System.Drawing.Point(589, 470);
+            this.txtBoxEstat.Name = "txtBoxEstat";
+            this.txtBoxEstat.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxEstat.Placeholder = null;
+            this.txtBoxEstat.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxEstat.TabIndex = 117;
+            this.txtBoxEstat.Tag = "Nom";
+            // 
+            // txtBoxAparell
+            // 
+            this.txtBoxAparell.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxAparell.Location = new System.Drawing.Point(589, 399);
+            this.txtBoxAparell.Name = "txtBoxAparell";
+            this.txtBoxAparell.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxAparell.Placeholder = null;
+            this.txtBoxAparell.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxAparell.TabIndex = 116;
+            this.txtBoxAparell.Tag = "Nom";
+            // 
+            // txtBoxUsuari
+            // 
+            this.txtBoxUsuari.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxUsuari.Location = new System.Drawing.Point(183, 466);
+            this.txtBoxUsuari.Name = "txtBoxUsuari";
+            this.txtBoxUsuari.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxUsuari.Placeholder = null;
+            this.txtBoxUsuari.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxUsuari.TabIndex = 115;
+            this.txtBoxUsuari.Tag = "Nom";
+            // 
+            // txtBoxIncidencies
+            // 
+            this.txtBoxIncidencies.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxIncidencies.Location = new System.Drawing.Point(183, 598);
+            this.txtBoxIncidencies.Name = "txtBoxIncidencies";
+            this.txtBoxIncidencies.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxIncidencies.Placeholder = null;
+            this.txtBoxIncidencies.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxIncidencies.TabIndex = 126;
+            this.txtBoxIncidencies.Tag = "Nom";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 602);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 125;
+            this.label6.Text = "Incidències:";
+            // 
+            // txtBoxNLot
+            // 
+            this.txtBoxNLot.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxNLot.Location = new System.Drawing.Point(589, 533);
+            this.txtBoxNLot.Name = "txtBoxNLot";
+            this.txtBoxNLot.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxNLot.Placeholder = null;
+            this.txtBoxNLot.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxNLot.TabIndex = 128;
+            this.txtBoxNLot.Tag = "Nom";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(492, 534);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 16);
+            this.label9.TabIndex = 127;
+            this.label9.Text = "Num. Lot:";
+            // 
+            // Sacrifici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 749);
+            this.Controls.Add(this.txtBoxNLot);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBoxIncidencies);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cBoxAtordiment);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBoxBox);
+            this.Controls.Add(this.txtBoxEstat);
+            this.Controls.Add(this.txtBoxAparell);
+            this.Controls.Add(this.txtBoxUsuari);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridAnimals);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label13);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Sacrifici";
+            this.Text = "Sacrifici";
+            this.Load += new System.EventHandler(this.Sacrifici_Load);
+            this.Controls.SetChildIndex(this.btnInserir, 0);
+            this.Controls.SetChildIndex(this.label13, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.gridAnimals, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.txtBoxUsuari, 0);
+            this.Controls.SetChildIndex(this.txtBoxAparell, 0);
+            this.Controls.SetChildIndex(this.txtBoxEstat, 0);
+            this.Controls.SetChildIndex(this.txtBoxBox, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.cBoxAtordiment, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtBoxIncidencies, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.txtBoxNLot, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.gridAnimals)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridAnimals;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label13;
+        private CustomControl.CustomTextBox txtBoxBox;
+        private CustomControl.CustomTextBox txtBoxEstat;
+        private CustomControl.CustomTextBox txtBoxAparell;
+        private CustomControl.CustomTextBox txtBoxUsuari;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private CustomControl.comboBox cBoxAtordiment;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private CustomControl.CustomTextBox txtBoxIncidencies;
+        private System.Windows.Forms.Label label6;
+        private CustomControl.CustomTextBox txtBoxNLot;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ escorxaDAM/GestioProcessos/Sacrifici.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BaseForm;
+using Models;
+
+namespace GestioProcessos
+{
+    public partial class Sacrifici : BaseInserir
+    {
+        escorxadam2Entities _m = new escorxadam2Entities();
+          
+        public Sacrifici()
+        {
+            InitializeComponent();
+        }
+
+        private void gridAnimals_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int num = e.RowIndex;
+
+            txtBoxEstat.Text = gridAnimals.Rows[num].Cells[0].Value.ToString();
+            txtBoxNLot.Text = gridAnimals.Rows[num].Cells[1].Value.ToString();
+
+        }
+
+
+        private void updateGrid()
+        {
+            //Agafo les dades
+            var dades = (from sacrifici in _m.sacrificis
+                         join lots in _m.lots on sacrifici.idSacrifici equals lots.idSacrifici
+                         join estats in _m.estat_sacrifici on sacrifici.idEstatSacrifici equals estats.idEstatSacrifici
+
+                         where (estats.nomEstatSacrifici == "En espera" || estats.nomEstatSacrifici == "En procés")
+                         select new { lots.numLot, estats.nomEstatSacrifici }).ToArray();
+
+            //Fico les dades
+            gridAnimals.DataSource = dades;
+        }
+
+        private void Sacrifici_Load(object sender, EventArgs e)
+        {
+            updateGrid();
+        }
+    }
+}
