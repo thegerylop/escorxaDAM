833fec5274a8f5445e2cd19e34f61ac3c8e81adc
Roger B <barrancosroger@gmail.com>
2018-05-22 18:27:40 +0200

    Pendent de continuar amb la combobox de lot

--- escorxaDAM/GestioProcessos/Processat Inicial.Designer.cs
+++ escorxaDAM/GestioProcessos/Processat Inicial.Designer.cs
@@ -28,77 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtBoxUsuari = new CustomControl.CustomTextBox();
-            this.txtBoxLot = new CustomControl.CustomTextBox();
-            this.txtBoxId = new CustomControl.CustomTextBox();
+            this.txtBoxPesCanal = new CustomControl.CustomTextBox();
+            this.txtBoxCarril = new CustomControl.CustomTextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gridAnimals = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
-            this.customTextBox1 = new CustomControl.CustomTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Usuaris = new CustomControl.comboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBoxEstat = new CustomControl.CustomTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFinalitzar = new System.Windows.Forms.Button();
+            this.cBoxLots = new CustomControl.comboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAnimals)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtBoxUsuari
+            // btnInserir
             // 
-            this.txtBoxUsuari.Location = new System.Drawing.Point(686, 367);
-            this.txtBoxUsuari.Name = "txtBoxUsuari";
-            this.txtBoxUsuari.Options = CustomControl.CustomTextBox.Items.Text;
-            this.txtBoxUsuari.Placeholder = null;
-            this.txtBoxUsuari.Size = new System.Drawing.Size(161, 20);
-            this.txtBoxUsuari.TabIndex = 102;
-            this.txtBoxUsuari.Tag = "Nom";
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
             // 
-            // txtBoxLot
+            // txtBoxPesCanal
             // 
-            this.txtBoxLot.Location = new System.Drawing.Point(686, 255);
-            this.txtBoxLot.Name = "txtBoxLot";
-            this.txtBoxLot.Options = CustomControl.CustomTextBox.Items.Text;
-            this.txtBoxLot.Placeholder = null;
-            this.txtBoxLot.Size = new System.Drawing.Size(161, 20);
-            this.txtBoxLot.TabIndex = 101;
-            this.txtBoxLot.Tag = "Nom";
+            this.txtBoxPesCanal.Location = new System.Drawing.Point(685, 410);
+            this.txtBoxPesCanal.Name = "txtBoxPesCanal";
+            this.txtBoxPesCanal.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxPesCanal.Placeholder = null;
+            this.txtBoxPesCanal.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxPesCanal.TabIndex = 102;
+            this.txtBoxPesCanal.Tag = "Nom";
             // 
-            // txtBoxId
+            // txtBoxCarril
             // 
-            this.txtBoxId.Location = new System.Drawing.Point(172, 363);
-            this.txtBoxId.Name = "txtBoxId";
-            this.txtBoxId.Options = CustomControl.CustomTextBox.Items.Text;
-            this.txtBoxId.Placeholder = null;
-            this.txtBoxId.Size = new System.Drawing.Size(161, 20);
-            this.txtBoxId.TabIndex = 100;
-            this.txtBoxId.Tag = "Nom";
+            this.txtBoxCarril.Location = new System.Drawing.Point(171, 406);
+            this.txtBoxCarril.Name = "txtBoxCarril";
+            this.txtBoxCarril.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxCarril.Placeholder = null;
+            this.txtBoxCarril.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxCarril.TabIndex = 100;
+            this.txtBoxCarril.Tag = "Nom";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(587, 367);
+            this.label8.Location = new System.Drawing.Point(586, 410);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(93, 16);
             this.label8.TabIndex = 99;
             this.label8.Text = "Pes del canal:";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(467, 259);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(213, 16);
-            this.label7.TabIndex = 98;
-            this.label7.Text = "Documentació d\'Identificació Boví:";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(57, 363);
+            this.label2.Location = new System.Drawing.Point(56, 406);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 16);
             this.label2.TabIndex = 97;
@@ -108,7 +96,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(100, 256);
+            this.label1.Location = new System.Drawing.Point(99, 299);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 16);
             this.label1.TabIndex = 96;
@@ -124,19 +112,20 @@
             this.label13.TabIndex = 104;
             this.label13.Text = "Processat Inicial";
             // 
-            // dataGridView1
+            // gridAnimals
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(291, 94);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(542, 90);
-            this.dataGridView1.TabIndex = 106;
+            this.gridAnimals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAnimals.Location = new System.Drawing.Point(291, 116);
+            this.gridAnimals.Name = "gridAnimals";
+            this.gridAnimals.Size = new System.Drawing.Size(261, 90);
+            this.gridAnimals.TabIndex = 106;
+            this.gridAnimals.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridAnimals_CellDoubleClick);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(136, 87);
+            this.panel1.Location = new System.Drawing.Point(136, 109);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(710, 108);
             this.panel1.TabIndex = 107;
@@ -151,48 +140,129 @@
             this.label3.TabIndex = 93;
             this.label3.Text = "Material en espera";
             // 
-            // customTextBox1
+            // Usuaris
             // 
-            this.customTextBox1.Location = new System.Drawing.Point(172, 252);
-            this.customTextBox1.Name = "customTextBox1";
-            this.customTextBox1.Options = CustomControl.CustomTextBox.Items.Text;
-            this.customTextBox1.Placeholder = null;
-            this.customTextBox1.Size = new System.Drawing.Size(161, 20);
-            this.customTextBox1.TabIndex = 108;
-            this.customTextBox1.Tag = "Nom";
+            this.Usuaris.FormattingEnabled = true;
+            this.Usuaris.Location = new System.Drawing.Point(171, 294);
+            this.Usuaris.Name = "Usuaris";
+            this.Usuaris.Reference = null;
+            this.Usuaris.Size = new System.Drawing.Size(161, 21);
+            this.Usuaris.TabIndex = 108;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(514, 549);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 26);
+            this.button1.TabIndex = 109;
+            this.button1.Text = "Mostra animals del lot";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // txtBoxEstat
+            // 
+            this.txtBoxEstat.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtBoxEstat.Location = new System.Drawing.Point(685, 294);
+            this.txtBoxEstat.Name = "txtBoxEstat";
+            this.txtBoxEstat.Options = CustomControl.CustomTextBox.Items.Text;
+            this.txtBoxEstat.Placeholder = null;
+            this.txtBoxEstat.ReadOnly = true;
+            this.txtBoxEstat.Size = new System.Drawing.Size(161, 20);
+            this.txtBoxEstat.TabIndex = 119;
+            this.txtBoxEstat.Tag = "Nom";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(638, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 118;
+            this.label4.Text = "Estat:";
+            // 
+            // btnFinalitzar
+            // 
+            this.btnFinalitzar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFinalitzar.BackColor = System.Drawing.Color.DimGray;
+            this.btnFinalitzar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFinalitzar.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.btnFinalitzar.ForeColor = System.Drawing.Color.White;
+            this.btnFinalitzar.Location = new System.Drawing.Point(730, 549);
+            this.btnFinalitzar.Name = "btnFinalitzar";
+            this.btnFinalitzar.Size = new System.Drawing.Size(132, 26);
+            this.btnFinalitzar.TabIndex = 131;
+            this.btnFinalitzar.Text = "Finalitzar";
+            this.btnFinalitzar.UseVisualStyleBackColor = false;
+            this.btnFinalitzar.Visible = false;
+            this.btnFinalitzar.Click += new System.EventHandler(this.btnFinalitzar_Click);
+            // 
+            // cBoxLots
+            // 
+            this.cBoxLots.FormattingEnabled = true;
+            this.cBoxLots.Location = new System.Drawing.Point(685, 36);
+            this.cBoxLots.Name = "cBoxLots";
+            this.cBoxLots.Reference = null;
+            this.cBoxLots.Size = new System.Drawing.Size(161, 21);
+            this.cBoxLots.TabIndex = 132;
+            this.cBoxLots.SelectionChangeCommitted += new System.EventHandler(this.cBoxLots_SelectionChangeCommitted);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(571, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 133;
+            this.label5.Text = "Número de Lot:";
             // 
             // Processat_Inicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 604);
-            this.Controls.Add(this.customTextBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cBoxLots);
+            this.Controls.Add(this.btnFinalitzar);
+            this.Controls.Add(this.txtBoxEstat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Usuaris);
+            this.Controls.Add(this.gridAnimals);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label13);
-            this.Controls.Add(this.txtBoxUsuari);
-            this.Controls.Add(this.txtBoxLot);
-            this.Controls.Add(this.txtBoxId);
+            this.Controls.Add(this.txtBoxPesCanal);
+            this.Controls.Add(this.txtBoxCarril);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "Processat_Inicial";
             this.Text = "Processat_Inicial";
+            this.Load += new System.EventHandler(this.Processat_Inicial_Load);
             this.Controls.SetChildIndex(this.btnInserir, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.label7, 0);
             this.Controls.SetChildIndex(this.label8, 0);
-            this.Controls.SetChildIndex(this.txtBoxId, 0);
-            this.Controls.SetChildIndex(this.txtBoxLot, 0);
-            this.Controls.SetChildIndex(this.txtBoxUsuari, 0);
+            this.Controls.SetChildIndex(this.txtBoxCarril, 0);
+            this.Controls.SetChildIndex(this.txtBoxPesCanal, 0);
             this.Controls.SetChildIndex(this.label13, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
-            this.Controls.SetChildIndex(this.dataGridView1, 0);
-            this.Controls.SetChildIndex(this.customTextBox1, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Controls.SetChildIndex(this.gridAnimals, 0);
+            this.Controls.SetChildIndex(this.Usuaris, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtBoxEstat, 0);
+            this.Controls.SetChildIndex(this.btnFinalitzar, 0);
+            this.Controls.SetChildIndex(this.cBoxLots, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.gridAnimals)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -201,17 +271,21 @@
         }
 
         #endregion
-        private CustomControl.CustomTextBox txtBoxUsuari;
-        private CustomControl.CustomTextBox txtBoxLot;
-        private CustomControl.CustomTextBox txtBoxId;
+        private CustomControl.CustomTextBox txtBoxPesCanal;
+        private CustomControl.CustomTextBox txtBoxCarril;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gridAnimals;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
-        private CustomControl.CustomTextBox customTextBox1;
+        private CustomControl.comboBox Usuaris;
+        public System.Windows.Forms.Button button1;
+        private CustomControl.CustomTextBox txtBoxEstat;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Button btnFinalitzar;
+        private CustomControl.comboBox cBoxLots;
+        private System.Windows.Forms.Label label5;
     }
 }
--- escorxaDAM/GestioProcessos/Processat Inicial.cs
+++ escorxaDAM/GestioProcessos/Processat Inicial.cs
@@ -8,14 +8,173 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BaseForm;
+using Models;
 
 namespace GestioProcessos
 {
     public partial class Processat_Inicial : BaseInserir
     {
+        escorxadam2Entities _m = new escorxadam2Entities();
+        string lot;
+        string estat;
+
         public Processat_Inicial()
         {
             InitializeComponent();
+        }
+
+        private void gridAnimals_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dadesDataGrid(e);
+            omplirCampsComuns();
+            if(estat.Equals("En espera"))
+            {
+                btnInserir.Visible = true;
+            }
+            else
+            {
+                btnFinalitzar.Visible = true;
+                omplirCampsProces();
+            }
+
+        }
+        private void omplirCampsComuns()
+        {
+            cBoxLots.Text = lot;
+            txtBoxEstat.Text = estat;
+            txtBoxPesCanal.Text = randomInt(320, 400).ToString();
+        }
+
+        private void omplirCampsProces()
+        {
+            var sacr = (from a in _m.lots
+                        join b in _m.sacrificis on a.idSacrifici equals b.idSacrifici
+                        where a.numLot == lot
+                        select b).FirstOrDefault();
+
+            txtBoxCarril.Text = randomInt(1,3).ToString();
+            //cBoxAtordiment.SelectedValue = sacr.idSistemaAtordiment;
+            //txtBoxIncidencies.Text = sacr.Incidencies;
+            Usuaris.SelectedValue = sacr.idUsuari;
+        }
+
+
+        private void dadesDataGrid(DataGridViewCellEventArgs e)
+        {
+            int num = e.RowIndex;
+            lot = gridAnimals.Rows[num].Cells[0].Value.ToString();
+            estat = gridAnimals.Rows[num].Cells[1].Value.ToString();
+        }
+
+        private void Processat_Inicial_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void updateGrid(string nLot)
+        {
+            //Agafo les dades
+            //var dades = (from sacrifici in _m.sacrificis
+            //             join lots in _m.lots on sacrifici.idSacrifici equals lots.idSacrifici
+            //             join estats in _m.estat_sacrifici on sacrifici.idEstatSacrifici equals estats.idEstatSacrifici
+
+            //             where (estats.nomEstatSacrifici == "En espera" || estats.nomEstatSacrifici == "En procés")
+            //             select new { lots.numLot, estats.nomEstatSacrifici }).ToArray();
+
+            var dades = (from animals in _m.animals
+                         join rb in _m.recepcions_bestiar on animals.idRecepcioBestiar equals rb.idRecepcio
+                         join ls in _m.lots on rb.idRecepcio equals ls.idRemo
+                         where (ls.numLot == nLot)
+                         select new { animals.DIB }).ToArray();
+                
+            //Fico les dades
+            gridAnimals.DataSource = dades;
+        }
+
+        public void obtenirUsuaris()
+        {
+            var usuaris = (from a in _m.usuaris
+                           select new { a.idUsuari, a.Nom }).ToArray();
+            Usuaris.DataSource = usuaris;
+            Usuaris.DisplayMember = "Nom";
+            Usuaris.ValueMember = "idUsuari";
+        }
+
+        
+        private void afegirBBDD()
+        {
+            using(escorxadam2Entities test = new escorxadam2Entities())
+            {
+                string idProcessatInicial = (from a in _m.lots
+                                             where a.numLot.ToString() == cBoxLots.Text
+                                             select a.idProcessatInicial).First().ToString();
+                var ProcessatInicial = (from a in _m.processats_inicials
+                                        where a.idProcessatInicial.ToString() == idProcessatInicial
+                                        select a).FirstOrDefault();
+
+                if(ProcessatInicial.idEstatInicial == 1)
+                {
+                    ProcessatInicial.idUsuari = Int32.Parse(Usuaris.SelectedValue.ToString());
+                    ProcessatInicial.idEstatInicial = 2;
+                    ProcessatInicial.numCarril = Int32.Parse(txtBoxCarril.Text);
+                    ProcessatInicial.pesCanal = Int32.Parse(txtBoxPesCanal.Text);
+                    MessageBox.Show("Inserit correctament");
+                }
+                else
+                {
+                    ProcessatInicial.idUsuari = Int32.Parse(Usuaris.SelectedValue.ToString());
+                    ProcessatInicial.idEstatInicial = 3;
+                }
+
+                _m.SaveChanges();
+               // updateGrid();
+            }
+        }
+        public int randomInt(int min, int max)
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(min, max);
+            return random;
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            omplirCampsProces();
+            afegirBBDD();
+        }
+
+        private void btnFinalitzar_Click(object sender, EventArgs e)
+        {
+            finalitzarProces();
+        }
+        public void finalitzarProces()
+        {
+            using(escorxadam2Entities enti = new escorxadam2Entities())
+            {
+                var proc = new emmagatzematges
+                {
+                    idUsuariEntrada = 1,
+                    idEstatRefrigeracio = 1,
+                };
+                _m.emmagatzematges.Add(proc);
+                _m.SaveChanges();
+                var lotes = _m.lots.OrderByDescending(u => u.numLot == cBoxLots.Text).FirstOrDefault();
+                long idEmm = (from a in _m.emmagatzematges
+                                    select a.idEstatRefrigeracio).Max();
+
+                lotes.idEmmagatzematge = idEmm;
+                _m.SaveChanges();
+            }
+            MessageBox.Show("Finalitzat");
+        
+    }
+
+        private void cBoxLots_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+           // updateGrid();
+            obtenirUsuaris();
         }
     }
 }
