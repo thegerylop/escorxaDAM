d0d5ccf8ed105c6f382ae4a9c4bec84ac1bbfb88
Daniel Fornas <dan1for0can@gmail.com>
2018-05-23 20:02:38 +0200

    Crystal Reports correcto (pre-codigo)

---
+++ escorxaDAM/EtiquetaSafata/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EtiquetaSafata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EtiquetaSafata")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("973c4e36-c9cf-4ffd-9d07-5a0f1f0011ab")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
